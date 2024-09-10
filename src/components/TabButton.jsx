33b04a5a51bed122a413344303108d7fc166f2e4
Matthias Hinojosa <hinojosamatthias@gmail.com>
2024-09-10 15:09:09 -0500

    The special children prop

---
+++ src/components/TabButton.jsx
@@ -0,0 +1,7 @@
+export default function TabButton({children}){
+  // export default function TabButton({label}){
+  return (
+    <li><button>{children}</button></li>
+    // <li><button>{label}</button></li>
+  )
+}
