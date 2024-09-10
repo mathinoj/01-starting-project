d659cdf5980570a3284833bc04fc911058da59d7
Matthias Hinojosa <hinojosamatthias@gmail.com>
2024-09-10 15:41:54 -0500

    Created click event handler

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,7 +1,13 @@
 export default function TabButton({children}){
   // export default function TabButton({label}){
+    function handleClick(){
+      console.log("herro");
+    }
+
   return (
-    <li><button>{children}</button></li>
+    <li>
+      <button onClick={handleClick}>{children}</button>
+    </li>
     // <li><button>{label}</button></li>
   )
 }
